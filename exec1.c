749fc541f190256b2a4592b72fb7063b816a26b9
SimchaTeich <sim.teich@gmail.com>
2024-05-09 10:26:59 -0400

    create program that execvp to ls

---
+++ exec1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/* exec1.c - shows how easy it is for a program to run a program
+ */
+main()
+{
+    char *arglist[3];
+
+    arglist[0] = "ls";
+    arglist[1] = "-l";
+    arglist[2] = 0;
+
+    printf("* * * About to exec ls -l\n");
+    execvp("ls", arglist);
+    printf("* * * ls is done. bye\n");
+}
