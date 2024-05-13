a386f93a420c8d346839dbe508f8038998837aa9
SimchaTeich <sim.teich@gmail.com>
2024-05-13 10:12:23 -0400

    write the headers

---
+++ chapter_09/smsh.h
@@ -0,0 +1,11 @@
+#define YES 1
+#define NO  0
+
+char *  next_cmd();
+char ** splitline(char * line);
+void    freelist(char ** list);
+void *  emalloc(size_t n);
+void *  erealloc(void *p, size_t n);
+int     execute(char ** arglist);
+void    fatal(char *s1, char *s2, int n);
+
