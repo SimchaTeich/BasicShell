f24c23f804847aae702abe6674f3468049c3a085
SimchaTeich <sim.teich@gmail.com>
2024-05-13 09:27:57 -0400

    write splitline function

--- chapter_09/splitline.c
+++ chapter_09/splitline.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include "smsh.h"
 
+#define is_delim(x) ((x)==' ' || (x)=='\t')
+
 /*
  * purpose: read next command line from fp
  * returns: synamiclly allocated string holding command line
@@ -51,5 +53,61 @@ char *next_cmd(char *prompt, FILE *fp)
 
     buf[pos] = '\0';
     return buf;
+}
+
+
+
+/*
+ * purpose: split a line into array of white-space seperated tokens
+ * returns: a NULL-terminated array of pointers to copies of the tokens
+ *          or NULL if line if no tokens on the line
+ *  action: traverse the array, locate strings, make copies
+ *    note: strtok() could work, but we may want to add quotes later
+ * */
+char ** splitline(char *line)
+{
+    char **args;
+    char spots    = 0;                    /* spots in table       */
+    int  bufspace = 0;                    /* bytes in table       */
+    int  argnum   = 0;                    /* slots used           */
+    char *cp      = line;                 /* pos in string        */
+    char *start;
+    int  len;
+
+    if (line == NULL)                     /* handle special case  */
+        return NULL;
+
+    args     = emalloc(BUFSIZ);           /* initialize array     */
+    bufspace = BUFSIZ;
+    spots    = BUFSIZ/sizeof(char *);
+
+    while (*cp != '\0')
+    {
+        while (is_delim(*cp))             /* skip leading spaces  */
+            ++cp;
+
+	if (*cp == '\0')                  /* quit at end-o-string */
+	    break;
+
+	/* make sure the array has room (+1 for NULL) */
+	if (argnum + 1 > spots)
+	{
+	    args = erealloc(args, bufspace + BUFSIZ);
+	    bufspace += BUFSIZ;
+	    spots += (BUFSIZ/sizeof(char *))
+	}
+
+	/* mark start, then find end of word */
+	start = cp;
+	len   = 1;
+	while (*++cp != '\0' && !(is_delim(*cp))
+	    ++len;
+
+	args[argnum] = newstr(start, len);
+	++argnum;
+    }
+
+    args[argnum] = NULL;
+    return args;
 }
 
