8d48c5c1c3a062e380ebe08839a117674379734d
SimchaTeich <sim.teich@gmail.com>
2024-05-26 03:37:53 -0400

    edit smsh2.c to smsh4.c

---
+++ chapter_09/smsh4.c
@@ -0,0 +1,59 @@
+/**  smsh4.c  small-shell version4
+ **             small shell that supports command line parsing
+ **             and if..then..else..fi logic (by calling process())
+ **             also, support environment variables
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "smsh.h"
+
+#define DFL_PROMPT "> "
+
+void setup();
+void fatal(char *s1, char *s2, int n);
+
+int main()
+{
+    char *cmdline, *prompt, **arglist;
+    int   result;
+
+    prompt = DFL_PROMPT;
+    setup();
+
+    while ((cmdline = next_cmd(prompt, stdin)) != NULL)
+    {
+        if ((arglist = splitline(cmdline)) != NULL)
+	{
+	    result = process(arglist);
+	    freelist(arglist);
+	}
+	free(cmdline);
+    }
+
+    return 0;
+}
+
+
+
+/*
+ * purpose: initialize shell
+ * returns: nothing. calls fatal() if trouble
+ * */
+void setup()
+{
+    extern char **environ;
+    VLenviron2table(environ);
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+}
+
+
+
+void fatal(char *s1, char *s2, int n)
+{
+    fprintf(stderr, "Error: %s, %s\n", s1, s2);
+    exit(n);
+}
+
