56dbbcdfc9d6ee83d545da2aaa7956ef2e74881c
SimchaTeich <sim.teich@gmail.com>
2024-05-26 08:10:49 -0400

    make it more readable

--- chapter_09/smsh.h
+++ chapter_09/smsh.h
@@ -1,11 +1,22 @@
 #define YES 1
 #define NO  0
 
-char *  next_cmd();
+/* splitline.c */
+char  * next_cmd();
 char ** splitline(char * line);
 void    freelist(char ** list);
 void *  emalloc(size_t n);
 void *  erealloc(void *p, size_t n);
+
+
+/* executeX.c  */
 int     execute(char ** arglist);
-void    fatal(char *s1, char *s2, int n);
+
+
+/* processX.c  */
 int     process(char **args);
+
+
+/* smshX.c     */
+void    fatal(char *s1, char *s2, int n);
+
