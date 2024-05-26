2c2f7f436180db715d9fe26c59d79b130c754c93
SimchaTeich <sim.teich@gmail.com>
2024-05-26 10:41:41 -0400

    write stdinredir1.c from 10.4.1

---
+++ chapter_10/stdinredir1.c
@@ -0,0 +1,41 @@
+/* stdinredir1.c
+ *      purpose: show hoe to redirect standatd input by relpacing file
+ *               descriptor 0 with a connection to a file.
+ *       action: reads three lines from standard input, then
+ *               closes fd 0, opens a disk file, then reads in
+ *               three more lines from standard input
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    int  fd;
+    char line[100];
+
+    /* read and print three lines */
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+
+    /* redirect input             */
+    close(0);
+    fd = open("/etc/passwd", O_RDONLY);
+    if (fd != 0)
+    {
+        fprintf(stderr, "Could not open data as fd 0\n");
+	exit(1);
+    }
+
+    /* read and print three lines */
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+    fgets(line, 100, stdin); printf("%s", line);
+
+    return 0;
+}
+
